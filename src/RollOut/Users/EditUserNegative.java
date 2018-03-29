bfbfa2cf8c27df000d1e310d7b1968bad421dda7
gsvk2734tp <gsvk2734tp@gmail.com>
2018-03-29 18:29:44 +0300

    Вынес метод из теста EditUserNegative в RollOutWeb

--- src/RollOut/Users/EditUserNegative.java
+++ src/RollOut/Users/EditUserNegative.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -35,14 +35,13 @@ public class EditUserNegative extends RollOutWeb {
 
     @Parameterized.Parameters
     public static List<Object> data() {
-        Object[] data = new Object[]{new ChromeDriver(), new FirefoxDriver()};
+        Object[] data = new Object[]{ new FirefoxDriver(), new EdgeDriver()};
         return Arrays.asList(data);
     }
 
 
     @Before
     public void setUp() throws InterruptedException {
-        //authSilso(URL_NSMS_SITE_TEST);
         driver.get(URL_NSMS_SITE);
         wait.until(titleIs(TITLE_APP));
         driver.get(URL_NSMS_USERS);
